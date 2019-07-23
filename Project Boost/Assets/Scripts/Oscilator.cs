0817d457a18570f56786c4ba9752f2b96cfb0ea9
jakubtomal <jakub.tomal@smcebi.edu.pl>
2019-07-23 21:23:12 +0200

    last

---
+++ Project Boost/Assets/Scripts/Oscilator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscilator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [Range(-1,1)][SerializeField] float movementFactor;
+    [SerializeField] float period = 2f;
+    private Vector3 startingPosition;
+
+    void Start()
+    {
+        startingPosition = transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(period > Mathf.Epsilon)
+        {
+            float cycle = Time.time / period;
+            const float tau = Mathf.PI * 2f;
+            float rowSinWave = Mathf.Sin(tau * cycle);
+            movementFactor = rowSinWave;
+            Vector3 offset = movementVector * movementFactor;
+            transform.position = startingPosition + offset;
+        }
+    }
+}
--- Project Boost/Assets/Scripts/RocketShip.cs
+++ Project Boost/Assets/Scripts/RocketShip.cs
@@ -2,12 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RocketShip : MonoBehaviour
 {
     [SerializeField] float flyingSpeed;
     [SerializeField] float rotationSpeed;
+    [SerializeField] float levelLoadDelay;
 
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip deathSound;
+    [SerializeField] AudioClip winSound;
+
+    [SerializeField] ParticleSystem engineParticle;
+    [SerializeField] ParticleSystem deathParticle;
+    [SerializeField] ParticleSystem winParticle;
+
+    private enum State {alive,dead,transcending}
+    private State state = State.alive;
 
     // Update is called once per frame
     void Update()
@@ -17,29 +29,60 @@ public class RocketShip : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-
+        if(state != State.alive) { return; }
         switch(collision.gameObject.tag)
         {
             case "Friendly":
                 break;
-            default:
-                Debug.Log("DIE");
+            case "Finish":
+                Win();
                 break;
-
+            default:
+                Die();
+                break;
         }
+    }
+    
+    private void Win()
+    {
+        gameObject.GetComponent<AudioSource>().Stop();
+        gameObject.GetComponent<AudioSource>().PlayOneShot(winSound);
+        winParticle.Play();
+        state = State.transcending;
+        Invoke("LoadNextScene", levelLoadDelay);
+    }
 
+    private void Die()
+    {
+        gameObject.GetComponent<AudioSource>().Stop();
+        gameObject.GetComponent<AudioSource>().PlayOneShot(deathSound);
+        deathParticle.Play();
+        state = State.dead;
+        Invoke("LoadCurrentScene", levelLoadDelay);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    private void LoadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void ProcessInput()
     {
+        if (state != State.alive) { return; }
+
         if (Input.GetKey(KeyCode.Space))
         {
             Fly(flyingSpeed);
         }
 
-        else if(Input.GetKeyUp(KeyCode.Space))
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
-            gameObject.GetComponent<AudioSource>().Stop();
+            StopFlying();
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
@@ -55,17 +98,27 @@ public class RocketShip : MonoBehaviour
 
     private void RotateOnZAxis(float speed)
     {
-        gameObject.GetComponent<Rigidbody>().freezeRotation = false;
-        gameObject.transform.Rotate(Vector3.forward * speed * Time.deltaTime);
         gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+        gameObject.transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+        gameObject.GetComponent<Rigidbody>().freezeRotation = false;
 
     }
 
     private void Fly(float speed)
     {
-        if (!gameObject.GetComponent<AudioSource>().isPlaying) { gameObject.GetComponent<AudioSource>().Play(); }
+        if (!gameObject.GetComponent<AudioSource>().isPlaying) { gameObject.GetComponent<AudioSource>().PlayOneShot(mainEngine); }
+        engineParticle.Play();
         gameObject.GetComponent<Rigidbody>().velocity += gameObject.transform.up * speed;
     }
+
+    private void StopFlying()
+    {
+        gameObject.GetComponent<AudioSource>().Stop();
+        engineParticle.Stop();
+    }
+
+
+
 
 
 
